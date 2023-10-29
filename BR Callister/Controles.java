e68b0cdc9788e207441c13608860f2bc75ace535
deivi <132165775+Davicbm@users.noreply.github.com>
2023-10-28 21:02:16 -0300

    Update Controles.java

--- BR Callister/Controles.java
+++ BR Callister/Controles.java
@@ -5,23 +5,24 @@ public class Controles implements KeyListener{
     public boolean cimaPress, baixoPress, esqPress, dirPress;
     @Override
     public void keyTyped(KeyEvent e) {
-        
+        switch (e.getKeyChar()) {
+            case 'w':
+                cimaPress = true;
+                break;
+            case 's':
+                baixoPress = true;
+                break;
+            case 'a': 
+                esqPress = true;
+                break;
+            case 'd':
+                dirPress = true;
+                break;
+        }
     }
     @Override
     public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
-        if (code == KeyEvent.VK_W);{
-            cimaPress = true;
-        }
-        if (code == KeyEvent.VK_S);{
-            baixoPress = true;
-        }
-        if (code == KeyEvent.VK_A);{
-            esqPress = true;
-        }
-        if (code == KeyEvent.VK_D);{
-            dirPress = true;
-        }
+        
     }
     @Override
     public void keyReleased(KeyEvent e) {
